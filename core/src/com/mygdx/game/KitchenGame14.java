3d8c63ed3f1b564601a2ddb8683e4a4813502144
HHllmn <harrythillman@gmail.com>
2022-11-30 13:32:49 +0000

    Added chef sprite and image

--- core/src/com/mygdx/game/KitchenGame14.java
+++ core/src/com/mygdx/game/KitchenGame14.java
@@ -4,15 +4,43 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 
 public class KitchenGame14 extends ApplicationAdapter {
-	SpriteBatch batch;
 	Texture img;
+
+	private Texture ChefImage; //Image of the chef variable
+	private Texture BGImage; //Image for the background
+	private OrthographicCamera camera; //Camera which will be used to view the game
+	private SpriteBatch batch; //Sprite batch which will use the ChefImage to display the character
+	public Rectangle ChefA; //Creates ChefA Rectangle/Hitbox
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		//Loads the images for Chef and Background (BG) in, 64x64 pixels each
+		//ChefImage = new Texture(Gdx.files.internal("ChefImage.png"));
+		//BGImage = new Texture(Gdx.files.internal("BGImage.png"));
+
+		//Creates the camera and sprite batch
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 960, 540); //Camera shows the whole screen (set to 960/540, desktop ratio)
+		batch = new SpriteBatch();
+
+		//Create Chef rectangle
+		ChefA = new Rectangle();
+		ChefA.x = 800 / 2 - 64 / 2;
+		ChefA.y = 20;
+		ChefA.width = 100;
+		ChefA.height = 50;
 	}
 
 	@Override
@@ -21,6 +49,16 @@ public class KitchenGame14 extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		ChefA.x = 100;
+		ChefA.y = 100;
+		Texture chefAtex = new Texture("chef.png");
+
+		batch.draw(chefAtex, ChefA.x, ChefA.y);
+		batch.end();
+
 	}
 	
 	@Override
